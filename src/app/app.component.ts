e0be7ce7842f6a4552a1d0eb8eaa47d56e6afaab
vishnu323 <44895109+vishnu323@users.noreply.github.com>
2023-09-07 02:11:52 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ExampleHeaderComponent } from './example-header/example-header.component';
-
+import { MatDateRangePicker } from '@angular/material/datepicker';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,11 +10,14 @@ import { ExampleHeaderComponent } from './example-header/example-header.componen
 export class AppComponent {
   // make ExampleHeaderComponent type available in our template:
   readonly ExampleHeaderComponent = ExampleHeaderComponent;
+  @ViewChild('picker') datePicker: MatDateRangePicker<Date>;
+  public handleDateRangeInputClick =() =>{
+    this.datePicker.open();
+    
+  }
 
   range = new FormGroup({
     start: new FormControl(),
     end: new FormControl(),
   });
-
-  isTouchUIActivated = false;
 }
